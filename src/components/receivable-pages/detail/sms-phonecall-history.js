daff2e3dfb3cc4a947c3c39e26340c03d89379e7
Nam Thanh Le <lethanhnam2882@gmail.com>
2019-04-30 15:42:51 +0700

    [func] Receivable detail: Make manual action for SMS and phone call

--- src/components/receivable-pages/detail/sms-phonecall-history.js
+++ src/components/receivable-pages/detail/sms-phonecall-history.js
@@ -2,6 +2,12 @@ import React from 'react';
 import { Modal, ModalHeader, ModalBody, ModalFooter, Table, Badge } from 'reactstrap';
 import { numAsDate, numAsTime } from '../../../utils/time-converter';
 import { Button, Divider, Label } from 'semantic-ui-react';
+import ConfirmModal from '../../modal/ConfirmModal';
+import { successAlert, errorAlert } from '../../common/my-menu';
+import { TabContent, TabPane, Nav, NavItem, NavLink, Card, CardTitle, CardText, Row, Col } from 'reactstrap';
+import classnames from 'classnames';
+import { AuthService } from '../../../services/auth-service';
+import { TaskService } from '../../../services/task-service';
 
 class SmsPhonecallHistory extends React.Component {
     constructor(props) {
@@ -9,20 +15,69 @@ class SmsPhonecallHistory extends React.Component {
         this.state = {
             modal: false,
             total: 0,
-            stages: this.props.stages
+            stages: this.props.stages,
+            openConfirm: false,
+            onConfirm: () => { this.setState({ openConfirm: false }) },
+            actionName: '',
+            showSuccess: true,
+            totalSuccess: 0,
+            totalNotSuccess: 0
         }
         this.showHistory = this.showHistory.bind(this);
         this.closeModel = this.closeModel.bind(this);
+        this.setOnConfirm = this.setOnConfirm.bind(this);
+        this.toggleShowSuccessAction = this.toggleShowSuccessAction.bind(this);
+        this.filterAction = this.filterAction.bind(this);
+        this.updateStages = this.updateStages.bind(this);
+    }
+    toggleShowSuccessAction() {
+        let showSuccess = !this.state.showSuccess;
+        this.setState({ showSuccess: showSuccess })
+        let stages = this.state.stages;
+        stages = this.filterAction(stages, showSuccess);
+        this.setState({ stages: stages });
+    }
+    setOnConfirm(actionName, onConfirm) {
+        this.setState({
+            openConfirm: true,
+            actionName: actionName,
+            onConfirm: () => {
+                onConfirm();
+                this.setState({ openConfirm: false })
+            }
+        })
     }
     componentDidMount() {
         let stages = this.state.stages;
-        stages.forEach(stage => {
-            stage.history_actions = stage.Actions.filter(a => a.Status !== 1 && (a.Type === 0 || a.Type === 1));
-            this.setState(pre => ({
-                total: pre.total + stage.history_actions.length
-            }))
-        })
+        stages = this.filterAction(stages);
         this.setState({ stages: stages });
+    }
+    filterAction(stages, showSuccess = this.state.showSuccess) {
+        let totalSuccess = 0;
+        let totalNotSuccess = 0;
+        stages.forEach(stage => {
+            stage.history_actions = stage.Actions.filter(a => {
+                let rsSuccess = a.Status !== 1 && a.Status === 2 && (a.Type === 0 || a.Type === 1);
+                if (rsSuccess) {
+                    totalSuccess++;
+                }
+                let rsNotSuccess = a.Status !== 1 && a.Status !== 2 && (a.Type === 0 || a.Type === 1)
+                if (rsNotSuccess) {
+                    totalNotSuccess++;
+                }
+                if (showSuccess) {
+                    return rsSuccess;
+                } else {
+                    return rsNotSuccess;
+                }
+            });
+        })
+        this.setState({
+            total: totalSuccess + totalNotSuccess,
+            totalNotSuccess: totalNotSuccess,
+            totalSuccess: totalSuccess
+        })
+        return stages;
     }
     showHistory(e) {
         e.preventDefault();
@@ -33,13 +88,31 @@ class SmsPhonecallHistory extends React.Component {
     closeModel() {
         this.setState({ modal: false });
     }
+    updateStages(stages = this.state.stages) {
+        this.setState({ stages: stages })
+    }
     render() {
         let stages = this.state.stages;
         let isEmpty = true;
         return (<div>
             <a href='' onClick={this.showHistory} style={{ float: 'left' }}><i>{`SMS and Phone call history (${this.state.total})`}</i></a>
             <Modal isOpen={this.state.modal} className='big-modal'>
-                <ModalHeader toggle={this.toggle}>{`SMS and Phone call history (${this.state.total})`}</ModalHeader>
+                <ModalHeader toggle={this.toggle}>
+                    <Nav tabs>
+                        <NavItem>
+                            <NavLink className={classnames({ active: this.state.showSuccess })}
+                                onClick={this.toggleShowSuccessAction}>
+                                {`Success SMS and Phone call history (${this.state.totalSuccess})`}
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink className={classnames({ active: !this.state.showSuccess })}
+                                onClick={this.toggleShowSuccessAction}>
+                                {`Fail SMS and Phone call history (${this.state.totalNotSuccess})`}
+                            </NavLink>
+                        </NavItem>
+                    </Nav>
+                </ModalHeader>
                 <ModalBody>
                     {stages.map((stage, i) => {
                         let actions = stage.history_actions;
@@ -60,18 +133,27 @@ class SmsPhonecallHistory extends React.Component {
                                     <tr>
                                         <th>Type</th>
                                         <th>Time</th>
+                                        <th>Count</th>
                                         <th>Status</th>
+                                        <th style={{ display: AuthService.isManager() ? 'none' : 'table-cell' }}></th>
                                     </tr>
                                 </thead>
                                 <tbody>
                                     {actions.map((action, i) => {
-                                        return <ActionRecord action={action} key={i} />
+                                        return <ActionRecord updateStages={this.updateStages} setOnConfirm={this.setOnConfirm} action={action} key={i} />
                                     })}
                                 </tbody>
                             </table>
                         </div>
                     })}
                     {isEmpty ? <span>Sorry, SMS and phone call history is empty!</span> : null}
+                    <ConfirmModal
+                        show={this.state.openConfirm}
+                        onHide={() => { this.setState({ openConfirm: false }) }}
+                        header='Confirm'
+                        body={`Do you want to resent this ${this.state.actionName}?`}
+                        callback={this.state.onConfirm}
+                    />
                 </ModalBody>
                 <ModalFooter>
                     <Button color="secondary" onClick={this.closeModel}>Close</Button>
@@ -86,13 +168,28 @@ class ActionRecord extends React.Component {
         super(props);
         this.state = {
         }
+        this.resendAction = this.resendAction.bind(this);
+    }
+
+    resendAction() {
+        let action = this.props.action;
+        TaskService.makeManualAction(action.Id).then(res => {
+            let action1 = res.data;
+            action.Note = action1.Note;
+            action.Status = action1.Status;
+            this.props.updateStages();
+            successAlert(`${action.Name} has been resent to debtor!`);
+        }).catch(err => {
+            console.error(err);
+            errorAlert('Something went wrong, please try again!');
+        })
     }
 
     render() {
         let action = this.props.action;
         let date = numAsDate(action.ExcutionDay);
         let time = numAsTime(action.StartTime);
-        let color = 'orange';
+        let color = 'red';
         switch (action.Status) {
             case 1:
                 color = 'gray'
@@ -107,18 +204,31 @@ class ActionRecord extends React.Component {
                 <span>At{` ${date} ${time}`}</span>
             </td>
             <td>
-                <Label color={color}>{describeActionStatus(action.Status)}</Label>
+                {action.Status != 2 ? '4' : '1'}
+                {/* {action.Note} */}
             </td>
+            <td>
+                <Label color={color}>{describeActionStatus(action.Status, true)}</Label>
+            </td>
+            <td style={{ display: AuthService.isManager() ? 'none' : 'table-cell' }}>{action.Status == 1 || action.Status == 3 ? <Button color='primary'
+                onClick={() => {
+                    this.props.setOnConfirm(action.Name, this.resendAction)
+                }}>Resend</Button> : null}</td>
         </tr>);
     }
 }
 
-const describeActionStatus = (status) => {
+const describeActionStatus = (status, smsOrPhonecall = false) => {
     switch (status) {
         case 1:
             return 'Not done';
         case 2: return 'Done';
-        default: return 'Late';
+        case 3:
+            if (smsOrPhonecall) {
+                return 'Cancel'
+            }
+            return 'Late';
+        default: return 'Cancel';
     }
 }
 
